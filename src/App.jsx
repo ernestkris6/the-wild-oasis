9734a0ef135de1917f7e799bed951dc4fbddff8e
ERNEST CHRIS <ernestchris6@gmail.com>
2025-04-30 09:27:52 +0100

    adding ui and other files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: 900;
+`
+
+const Button = styled.button`
+background-color: purple;
+color: white;
+`
+
+const StyledApp = styled.div`
+  background-color: green;
+`
 
 export default function App() {
   return (
-    <div>App</div>
+    <StyledApp>
+      <H1>App</H1>
+      
+      <Button onClick={()=> alert('checked in? What is your name??')}>Check in</Button>
+    </StyledApp>
   )
 }
