187eea4d72ee0cc5c3184aa25a5eacae74abe4df
ERNEST CHRIS <ernestchris6@gmail.com>
2025-04-30 12:11:43 +0100

    adding other ui

---
+++ src/ui/SpinnerMini.jsx
@@ -0,0 +1,16 @@
+import styled, { keyframes } from "styled-components";
+import { BitLoaderAlt } from 'react-icons/bi';
+
+const rotate = keyframes`
+    to {
+        transform: rotate(1turn);
+    }
+`;
+
+const SpinnerMini = styled(BitLoaderAlt)`
+    width: 2.4rem;
+    height: 2.4rem;
+    animation: ${rotate} 1.5s infinite linear;
+`;
+
+export default SpinnerMini;
