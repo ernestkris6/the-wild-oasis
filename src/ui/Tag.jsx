71e6505d7c62f086c2060e8db21af483f0b368e3
ERNEST CHRIS <ernestchris6@gmail.com>
2025-07-22 16:57:23 +0100

    tag

--- src/ui/Tag.jsx
+++ src/ui/Tag.jsx
@@ -5,7 +5,7 @@ const Tag = styled.span`
     text-transform: uppercase;
     font-size: 1.1rem;
     font-weight: 600;
-    padding: 0%.4rem 1.2rem;
+    padding: 0.4rem 1.2rem;
     border-radius: 100px;
 
     /*Make these dynamic, based on the received prop*/
