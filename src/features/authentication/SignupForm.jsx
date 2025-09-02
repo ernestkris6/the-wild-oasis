4ce1a023b91455dbbba9253cb147b9fe9ad99275
ERNEST CHRIS <ernestchris6@gmail.com>
2025-09-02 12:08:15 +0100

    user sign up

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -3,50 +3,78 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useSignUp } from "./useSignUp";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
-
-  const { register, formState, getValues, handleSubmit } = useForm();
+  
+  const {signUp, isLoading} = useSignUp();
+  const { register, formState, getValues, handleSubmit, reset } = useForm();
 
   const { errors } = formState;
 
-  function onSubmit(data) {
-    console.log(data);
+
+  // function onSubmit(data) {
+  //   console.log(data);
     
+  // }
+
+  function onSubmit({fullName, email, password}) {
+      signUp({fullName, email,password}, 
+        {
+        onSettled: () => reset(),
+        }
+      )
   }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
       <FormRow label="Full name" error={errors?.fullName?.message}>
-        <Input type="text" id="fullName" {...register("fullName", {required: "This field is required"})}/>
+        <Input type="text" 
+        id="fullName" 
+        disabled={isLoading}
+        {...register("fullName", {
+          required: "This field is required"})} 
+          />
       </FormRow>
 
       <FormRow label="Email address" error={errors?.email?.message}>
         <Input type="email" 
-          id="email" {...register("email", {
+          id="email" 
+          disabled={isLoading}
+          {...register("email", {
           required: "This field is required", 
           pattern: {
             value: /\S+@\S+\.\S+/,
             message: "Please provide a valid email address",
-          }})} />
+          }})} 
+          />
       </FormRow>
 
       <FormRow label="Password (min 8 characters)" error={errors?.password?.message}>
-        <Input type="password" id="password" {...register("password", {
+        <Input type="password" 
+          id="password" 
+          disabled={isLoading}
+          {...register("password", {
           required: "This field is required",
           minLength: {
             value: 8,
             message: 'Password needs a minimum of 8 characters'
           },
-         })}/>
+         })}
+         
+         />
       </FormRow>
 
       <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
-        <Input type="password" id="passwordConfirm" {...register("passwordConfirm", {
+        <Input type="password" 
+        id="passwordConfirm" 
+        disabled={isLoading}
+        {...register("passwordConfirm", {
           required: "This field is required",
-          validate: (value) => value === getValues().password || 'Passwords need to match' })}/>
+          validate: (value) => value === getValues().password || 'Passwords need to match' })}
+          />
       </FormRow>
 
       <FormRow>
@@ -54,7 +82,7 @@ function SignupForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Create new user</Button>
+        <Button disabled={isLoading}>{!isLoading ? 'Create new user' : 'Creating new user'}</Button>
       </FormRow>
     </Form>
   );
---
+++ src/features/authentication/useSignUp.js
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { signUp as signUpApi } from "../../services/apiAuth";
+import { toast } from "react-hot-toast";
+
+export function useSignUp() {
+    const {mutate: signUp, isLoading} = useMutation({
+    mutationFn: signUpApi,
+    onSuccess: (user) => {
+        console.log(user);
+        toast.success('Account successfully created! Please verify the new account from the user\'s email address')
+        
+    },
+    onError: (error) => {
+        toast(error.message)
+    }
+})
+
+    return {signUp, isLoading}
+}
